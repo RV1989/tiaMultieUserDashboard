e67ce10d8c33aa04b00490b902286f5e61fec96f
RV1989 <rien__verbeke@msn.com>
2019-06-05 22:25:01 +0200

    insert projects if not existing

--- getMultieUserData.js
+++ getMultieUserData.js
@@ -7,14 +7,18 @@ let multieUserDefaultPath = './testData'
 const getProjects = async function(multieUserPath){
 let dbProjects =  await sqlite.open(path.join(multieUserPath,'/db','/projects.db'))
 const db = await sqlite.open('./database.db')
-db.run(`CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT`)
+await db.run(`CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT)`)
 
 var get = `SELECT * FROM Projects `;
 var projects = await dbProjects.all(get);
 for (let project of projects){
     let query = `SELECT name FROM projects WHERE name = "${project.ProjectName}"`
-    let exist = await db.ProjectNameget(query )
+    let exist = await db.get(query )
     console.log(exist)
+    if(!exist){
+        await db.run(`INSERT INTO projects (name) VALUES ("${project.ProjectName}")`)
+    }
+    console.log(project.ProjectName)
 }
 
 }
